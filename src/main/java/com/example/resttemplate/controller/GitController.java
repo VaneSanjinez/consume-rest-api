d463069fbf938aa9e0a4d85458209e64fdebe882
Vanessa Sanjinez <vane.sanji@gmail.com>
2020-05-17 21:13:57 -0400

    Add three endpoints to get list of json objects

--- src/main/java/com/example/resttemplate/controller/GitController.java
+++ src/main/java/com/example/resttemplate/controller/GitController.java
@@ -1,18 +1,21 @@
 package com.example.resttemplate.controller;
 
 import com.example.resttemplate.util.GitUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 //@RequestMapping(path ="/api")
 //@RequestMapping(path = "${v1API}/")
-@Api(value = "Gitlab microservice", description = "This API has a CRUD to consume GITLAB API")
+//@Api(value = "Gitlab microservice", description = "This API has a CRUD to consume GITLAB API")
 public class GitController {
 
     @Autowired
@@ -20,7 +23,7 @@ public class GitController {
 
 
     @RequestMapping(value="/hello")
-    @ApiOperation(value = "Test controller", notes = "Return a greetings String " )
+//    @ApiOperation(value = "Test controller", notes = "Return a greetings String " )
     public String hello(){
         return "Hi!";
     }
@@ -32,4 +35,33 @@ public class GitController {
         System.out.println(response);
         return response;
     }
+
+    @RequestMapping(value="get/projectsJsonObject" , method= RequestMethod.GET)
+    public Object[] getProjectsByUserId1(){
+        Object[] getResponse = null;
+        getResponse = gitUtil.getProjectsByUserNameJSONList("vane-sanjinez","cxXdxSAm8KmZZe7RZ7i6");
+//        System.out.println(getResponse.getStatusCode());
+        System.out.println(getResponse.length);
+        return getResponse;
+    }
+
+    @RequestMapping(value="get/responseEntity" , method= RequestMethod.GET)
+    public ResponseEntity<Object[]> getProjectsByUserId2(){
+        ResponseEntity<Object[]>getResponse = null;
+        getResponse = gitUtil.getProjectsByUserNameResponseEntity("vane-sanjinez","cxXdxSAm8KmZZe7RZ7i6");
+//        System.out.println(getResponse.getStatusCode());
+        System.out.println(getResponse.getStatusCode());
+        System.out.println(getResponse.getBody());
+        System.out.println("from git controller!");
+        return getResponse;
+    }
+
+
+    @RequestMapping(value="/object/getList/", method=RequestMethod.GET)
+    public @ResponseBody List<Object> findAllObjects() {
+        List<Object> objects = new ArrayList<Object>();
+        objects = Arrays.asList(gitUtil.getProjectsByUserNameJSONList("vane-sanjinez", "cxXdxSAm8KmZZe7RZ7i6"));
+        return objects;
+    }
+
 }
